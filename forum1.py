4faa807b3822e6bb2bed614cc71e2d767e46f8ab
sodokuApp <605889796@qq.com>
2018-04-08 08:33:25 -0500

    Add files via upload
    login and regist

---
+++ forum1.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask import render_template,request,redirect,url_for,session
+from models import User
+from exts import db
+
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login/',methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        return render_template('login.html')
+    else:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter(User.email == email,User.password == password).first()
+        if user:
+            session['user_id']=user.id
+            #如果想在31天内都不需要登录
+            session.permanent=True
+            return redirect(url_for('index'))
+        else:
+            return u'email or password is invalid'
+
+@app.route('/regist/',methods=["GET","POST"])
+def regist():
+    if request.method=="GET":
+        return render_template('regist.html')
+    else:
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        #check whether the email is already registed
+        user=User.query.filter(User.email==email).first()
+        if user:
+            return u'This email is already registed. Please change another one'
+        else:
+    # check whether two passwords are the same
+            if password1!=password2:
+                return u'Passwords are not the same.'
+            else:
+                user=User(email=email,username=username,password=password1)
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('login'))
+
+
+
+if __name__ == '__main__':
+    app.run()
