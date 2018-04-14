0358c21d2c27791024a0f573d3d3663555bc3a33
Susan <yaojianjing@126.com>
2018-04-14 22:24:44 +0800

    Update models.py

--- models.py
+++ models.py
@@ -6,3 +6,13 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False, unique = True)
     name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    
+class Question(db.Model):
+    __tablename__ = 'question'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    create_time = db.Column(db.DateTime, default = datetime.now)
+    author_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    author = db.relationship('User',backref =  db.backref('questions'))
