4b71d5a44b30b73f445924b3703d83262189cdfd
sodokuApp <605889796@qq.com>
2018-04-16 09:59:03 +0800

    Update forum1.py

--- forum1.py
+++ forum1.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import render_template,request,redirect,url_for,session
-from models import User
+from models import User,Question,Answer
 from exts import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from decorators import login_required
 
 import config
 
@@ -13,7 +14,10 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    context = {
+        'questions': Question.query.order_by('-create_time').all()
+    }
+    return render_template('index.html',**context)
 
 @app.route('/login/',methods=["GET","POST"])
 def login():
@@ -37,10 +41,10 @@ def login():
         else:
             return u'The email is invalid.'
 
-@app.route('/register/',methods=["GET","POST"])
-def register():
+@app.route('/regist/',methods=["GET","POST"])
+def regist():
     if request.method=="GET":
-        return render_template('register.html')
+        return render_template('regist.html')
     else:
         email = request.form.get('email')
         username = request.form.get('username')
@@ -52,7 +56,7 @@ def register():
         if user:
             return u'This email is already registed. Please change another one'
         else:
-        # check whether two passwords are the same
+    # check whether two passwords are the same
             if password1!=password2:
                 return u'Passwords are not the same.'
             else:
@@ -62,7 +66,45 @@ def register():
                 db.session.commit()
                 return redirect(url_for('login'))
 
+@app.route('/question/',methods=['GET','POST'])
+@login_required
+def question():
+    if  request.method == 'GET':
+        return render_template('question.html')
+    else:
+        title = request.form.get('title')
+        content = request.form.get('content')
+        question = Question(title=title,content=content)
+        user_id=session.get('user_id')
+        user=User.query.filter(User.id==user_id).first()
+        question.author =user
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('index'))
 
+@app.route('/detail/<question_id>/')
+def detail(question_id):
+    question_model = Question.query.filter(Question.id==question_id).first()
+    return render_template('detail.html',question=question_model)
+
+@app.route('/add_answer/',methods=['POST'])
+@login_required
+def add_answer():
+    question_id = request.form.get('question_id')
+    content = request.form.get('answer_content')
+    answer = Answer(content=content)
+    user_id = session.get('user_id')
+    user = User.query.filter(User.id == user_id).first()
+    answer.author = user
+    answer.question = Question.query.filter(Question.id==question_id).first()
+    db.session.add(answer)
+    db.session.commit()
+    return redirect(url_for('detail',question_id=question_id))
+
+@app.route('/logout/',methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
