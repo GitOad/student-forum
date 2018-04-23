e4db6516f8959ddd91ca9af9da2c43561bdc87e2
Susan <yaojianjing@126.com>
2018-04-23 23:20:30 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,22 +1,31 @@
 from exts import db
 from datetime import datetime
+from datetime import date
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer,primary_key = True, autoincrement=True)
-    email = db.Column(db.String(100), nullable=False, unique = True)
-    name = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    
+    id = db.Column(db.Integer,primary_key = True, autoincrement = True)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    username = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+
+    number_of_post = db.Column(db.Integer)
+    number_of_comment = db.Column(db.Integer)
+    point = db.Column(db.Integer)
+    grade = db.Column(db.Integer)
+    friends = db.Column(db.Text, nullable=False)
+    register_time = db.Column(db.DateTime, default = datetime.now)
+    last_login_time = db.Column(db.DateTime, default = datetime.now)
+
 class Question(db.Model):
     __tablename__ = 'question'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(100), nullable = False)
     content = db.Column(db.Text, nullable = False)
     create_time = db.Column(db.DateTime, default = datetime.now)
     author_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
-    author = db.relationship('User',backref =  db.backref('questions'))
+    author = db.relationship('User',backref = db.backref('questions'))
 
 class Answer(db.Model):
     __tablename__ = 'answer'
@@ -28,3 +37,20 @@ class Answer(db.Model):
 
     question = db.relationship('Question',backref = db.backref('answers',order_by = id.desc()))
     author = db.relationship('User',backref = db.backref('answers'))
+
+class Information(db.Model):
+    __tablename__ = 'info'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    gender = db.Column(db.String(100), nullable = False)
+    birthday = db.Column(db.String(100), nullable = False)
+    age = db.Column(db.Integer)
+    major = db.Column(db.String(100), nullable = False)
+    group = db.Column(db.String(100), nullable = False) #i.e. class
+    hobbies = db.Column(db.Text, nullable = False)
+    introduction = db.Column(db.Text, nullable=False)
+
+    #photo = db.Column(db.LargeBinary(length=2048))  # 以二进制形式存储图片
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #foreign key (connect to the table "user")
+
+    owner = db.relationship('User',backref = db.backref('information'))
