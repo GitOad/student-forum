d43b0ddfb10e0806a1ab59dc878dfcd43f956fba
sodokuApp <605889796@qq.com>
2018-05-17 23:14:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -16,3 +16,20 @@ SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}".format(DIALECT,DRIVER,USERNAM
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 WHOOSH_BASE='path/to/whoosh/base' 
+
+SECURITY_PASSWORD_SALT = 'my_precious_two'
+# mail settings
+MAIL_SERVER = 'smtp.126.com'
+MAIL_PROT = 25
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+
+# gmail authentication
+# MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+# MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+MAIL_USERNAME = 'forumaces@126.com'
+MAIL_PASSWORD = 'aces6666'
+MAIL_DEBUG = True
+
+# mail accounts
+MAIL_DEFAULT_SENDER = 'from@example.com'
