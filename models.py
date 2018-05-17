41c021d3fc5238e68ee59386813b2f71e5b143a0
Susan <yaojianjing@126.com>
2018-05-17 22:02:45 +0800

    Update models.py

--- models.py
+++ models.py
@@ -15,26 +15,35 @@ class User(db.Model):
     number_of_comment = db.Column(db.Integer)
     point = db.Column(db.Integer)
     grade = db.Column(db.Integer)
-    friends = db.Column(db.Text, nullable=False)
     register_time = db.Column(db.DateTime, default = datetime.now)
     last_login_time = db.Column(db.DateTime)
+    introduction = db.Column(db.Text)
+
+    photo = db.Column(db.String(100), nullable=True)  # 存储图片的路径
 
 class Question(db.Model):
     __tablename__ = 'question'
-    __searchable__ = ['content', 'title']   
-    __analyzer__ = ChineseAnalyzer()   
+    __searchable__ = ['content', 'title']
+    __analyzer__ = ChineseAnalyzer()
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(100), nullable = False)
     content = db.Column(db.Text, nullable = False)
+    # type = db.Column(db.Text, nullable = False) #帖子的分类
     create_time = db.Column(db.DateTime, default = datetime.now)
     author_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
+
     author = db.relationship('User',backref = db.backref('questions'))
+
+    def __repr__(self):
+        return '{0}(title={1})'.format(self.__class__.__name__, self.title)
+
 
 class Answer(db.Model):
     __tablename__ = 'answer'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     content = db.Column(db.Text, nullable = False)
+    # total_reported_time = db.Column(db.Integer)  #被举报的总数
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     create_time = db.Column(db.DateTime, default = datetime.now)
@@ -46,15 +55,22 @@ class Information(db.Model):
     __tablename__ = 'info'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    gender = db.Column(db.String(100), nullable = False)
-    birthday = db.Column(db.String(100), nullable = False)
+    gender = db.Column(db.String(100))
+    birthday = db.Column(db.String(100))
     age = db.Column(db.Integer)
-    major = db.Column(db.String(100), nullable = False)
-    group = db.Column(db.String(100), nullable = False) #i.e. class
-    hobbies = db.Column(db.Text, nullable = False)
-    introduction = db.Column(db.Text, nullable=False)
+    major = db.Column(db.String(100))
+    group = db.Column(db.String(100)) #i.e. class
+    hobbies = db.Column(db.Text)
 
-    #photo = db.Column(db.LargeBinary(length=2048))  # 以二进制形式存储图片
+    number_of_followed = db.Column(db.Integer) #粉丝总数
+    number_of_following = db.Column(db.Integer) #关注总数
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #foreign key (connect to the table "user")
 
     owner = db.relationship('User',backref = db.backref('information'))
+
+class Following(db.Model):
+    __tablename__ = 'follow'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer)
+    followed_user_id = db.Column(db.Integer)
